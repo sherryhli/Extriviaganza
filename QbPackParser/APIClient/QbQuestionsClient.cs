ad8d54f95e67058b3ff584a22505a415d68a6b53
Sherry Li <sherryhli@users.noreply.github.com>
2019-08-27 20:25:15 -0400

    Fix API client in QbPackParser

--- QbPackParser/APIClient/QbQuestionsClient.cs
+++ QbPackParser/APIClient/QbQuestionsClient.cs
@@ -1,4 +1,8 @@
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.KeyVault.Models;
+using Microsoft.Azure.Services.AppAuthentication;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,8 +22,32 @@ namespace QbPackParser.APIClient
         /// <param name="content">The questions to add in the form of a JSON array</param>
         public async Task<HttpResponseMessage> AddQuestionsToDbAsync(string content)
         {
+            HttpResponseMessage authorizationResponse = await GetAuthorization();
+            string token = await authorizationResponse.Content.ReadAsStringAsync();
+            DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
             StringContent stringContent = new StringContent(content, Encoding.UTF8, "application/json");
-            return await PostAsync("qbquestions", stringContent);
+            return await PostAsync("api/qbquestions", stringContent);
+        }
+
+        // TODO: Refactor to use refresh token
+        /// <summary>Gets the authorization information.</summary>
+        private async Task<HttpResponseMessage> GetAuthorization()
+        {
+            string username = await GetKeyVaultSecret("https://extriviaganza-vault.vault.azure.net/secrets/QbQuestionsUsername");
+            string password = await GetKeyVaultSecret("https://extriviaganza-vault.vault.azure.net/secrets/QbQuestionsPassword");
+            string content = "{\"username\":\"" + username + "\",\"password\":\"" + password + "\"}";
+            StringContent stringContent = new StringContent(content, Encoding.UTF8, "application/json");
+            return await PostAsync("api/authenticate", stringContent);
+        }
+
+        /// <summary>Retrieves a secret from Azure Key Vault</summary>
+        /// <param name="secretId">The secret's identifier in Azure Key Vault</param>
+        private async Task<string> GetKeyVaultSecret(string secretId)
+        {
+            AzureServiceTokenProvider azureServiceTokenProvider = new AzureServiceTokenProvider();
+            KeyVaultClient keyVaultClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+            SecretBundle secret = await keyVaultClient.GetSecretAsync(secretId).ConfigureAwait(false);
+            return secret.Value;
         }
     }
 }
